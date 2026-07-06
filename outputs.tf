47b2f0f27d19b8a478ed2cb499eedcb58ed08415
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,8 @@ output "portal_tenant_configurations" {
   description = "All portal_tenant_configuration resources"
   value       = azurerm_portal_tenant_configuration.portal_tenant_configurations
 }
+output "portal_tenant_configurations_private_markdown_storage_enforced" {
+  description = "List of private_markdown_storage_enforced values across all portal_tenant_configurations"
+  value       = [for k, v in azurerm_portal_tenant_configuration.portal_tenant_configurations : v.private_markdown_storage_enforced]
+}
 
