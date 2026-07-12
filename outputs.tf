d25f919daf890baf0f3d9a5352475a9c751d19f0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "portal_tenant_configurations_id" {
+  description = "Map of id values across all portal_tenant_configurations, keyed the same as var.portal_tenant_configurations"
+  value       = { for k, v in azurerm_portal_tenant_configuration.portal_tenant_configurations : k => v.id }
+}
 output "portal_tenant_configurations_private_markdown_storage_enforced" {
   description = "Map of private_markdown_storage_enforced values across all portal_tenant_configurations, keyed the same as var.portal_tenant_configurations"
   value       = { for k, v in azurerm_portal_tenant_configuration.portal_tenant_configurations : k => v.private_markdown_storage_enforced }
